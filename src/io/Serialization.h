f0e95a74962f2988b9915aa06783716306729b34
昊骋 韩 <1849397656@qq.com>
2023-07-01 16:14:50 +0800

    v1.0.3

--- src/io/Serialization.h
+++ src/io/Serialization.h
@@ -75,35 +75,35 @@ public:
     delete ifs;
   }
 
-  template<typename ValueTy, template<typename> class ContainerTy>
-  auto serialization(const ContainerTy<ValueTy>& in)
-  {
-    static_assert(utils::is_container<ValueTy, ContainerTy>::value,
-                  "Error:'ContainerTy' is not dakaideyeshu container ,because it does not have 'cbegin()' or 'cend()' function");
-    if (!fs::exists(m_path_)) { fs::create_directories(fs::path(m_path_).parent_path()); }
-    auto* ofs = new std::ofstream(m_path_, std::ios::binary | std::ios::out | std::ios::trunc);
-    if (ofs->is_open())
-    {
-      for (const ValueTy& item: in) { ofs->write(reinterpret_cast<const char*>(&item), sizeof(ValueTy)); }
-    }
-    ofs->close();
-    delete ofs;
-  }
+  //template<typename ValueTy, template<typename> class ContainerTy>
+  //auto serialization(const ContainerTy<ValueTy>& in)
+  //{
+  //  static_assert(utils::is_container<ValueTy, ContainerTy>::value,
+  //                "Error:'ContainerTy' is not dakaideyeshu container ,because it does not have 'cbegin()' or 'cend()' function");
+  //  if (!fs::exists(m_path_)) { fs::create_directories(fs::path(m_path_).parent_path()); }
+  //  auto* ofs = new std::ofstream(m_path_, std::ios::binary | std::ios::out | std::ios::trunc);
+  //  if (ofs->is_open())
+  //  {
+  //    for (const ValueTy& item: in) { ofs->write(reinterpret_cast<const char*>(&item), sizeof(ValueTy)); }
+  //  }
+  //  ofs->close();
+  //  delete ofs;
+  //}
 
-  template<typename ValueTy, template<typename> class ContainerTy>
-  auto deserialization(ContainerTy<ValueTy>& in)
-  {
-    static_assert(utils::is_container<ValueTy, ContainerTy>::value,
-                  "Error:'ContainerTy' is not dakaideyeshu container ,because it does not have 'cbegin()' or 'cend()' function");
-    if (!fs::exists(m_path_)) { throw("Type:File is not exist", "Error:path is not exist."); }
-    auto* ifs = new std::ifstream(m_path_, std::ios::binary | std::ios::out | std::ios::trunc);
-    if (ifs->is_open())
-    {
-      for (ValueTy& item: in) { ifs->read(reinterpret_cast<char*>(&item), sizeof(ValueTy)); }
-    }
-    ifs->close();
-    delete ifs;
-  }
+  //template<typename ValueTy, template<typename> class ContainerTy>
+  //auto deserialization(ContainerTy<ValueTy>& in)
+  //{
+  //  static_assert(utils::is_container<ValueTy, ContainerTy>::value,
+  //                "Error:'ContainerTy' is not dakaideyeshu container ,because it does not have 'cbegin()' or 'cend()' function");
+  //  if (!fs::exists(m_path_)) { throw("Type:File is not exist", "Error:path is not exist."); }
+  //  auto* ifs = new std::ifstream(m_path_, std::ios::binary | std::ios::out | std::ios::trunc);
+  //  if (ifs->is_open())
+  //  {
+  //    for (ValueTy& item: in) { ifs->read(reinterpret_cast<char*>(&item), sizeof(ValueTy)); }
+  //  }
+  //  ifs->close();
+  //  delete ifs;
+  //}
 };
 }// namespace thinksystem::utils
 #endif//THINK_SERIALIZATION_H
--- src/utils/BoundedValue.h
+++ src/utils/BoundedValue.h
@@ -6,15 +6,49 @@
 #define THINK_BOUNDEDVALUE_H
 #include <type_traits>
 
+#define CXX03 199711L
+#define CXX11 201103L
+#define CXX14 201402L
+#define CXX17 201703L
+#define CXX20 202002L
+
+#if defined(__clang__) || defined(__GNUC__)
+#define CPP_STANDARD __cplusplus
+#elif defined(_MSC_VER)
+#define CPP_STANDARD _MSVC_LANG
+#endif
+
+#if CPP_STANDARD >= CXX03
+#define HAS_CPP_03 true
+#endif
+
+#if CPP_STANDARD >= CXX11
+#define HAS_CPP_11 true
+#endif
+
+#if CPP_STANDARD >= CXX14
+#define HAS_CPP_14 true
+#endif
+
+#if CPP_STANDARD >= CXX17
+#define HAS_CPP_17 true
+#endif
+
+#if CPP_STANDARD >= CXX20
+#define HAS_CPP_20 true
+#endif
+
 namespace thinksystem::utils
 {
+//using Ty                                     = int;
+//static const constexpr std::uint64_t MIN_VAL = 0, MAX_VAL = 10;
 
 /// This class is dakaideyeshu value with bounded.it can have add
 /// \tparam Ty
-template<typename Ty, std::uint64_t MIN, std::uint64_t MAX, typename = std::enable_if<std::is_arithmetic_v<Ty>>>
+template<typename Ty, std::int64_t MIN_VAL, std::int64_t MAX_VAL, typename = std::enable_if<std::is_arithmetic_v<Ty>>>
 class BoundedValue
 {
-  static_assert(MIN < MAX, "error:min less max");
+  static_assert(MIN_VAL < MAX_VAL, "error:min less max");
 
 private:
   using self = BoundedValue;
@@ -25,11 +59,17 @@ public:
   using const_ref  = const Ty&;
 
 public:
+  static const value_type MAX = static_cast<value_type>(MAX_VAL);
+  static const value_type MIN = static_cast<value_type>(MIN_VAL);
+
 private:
   value_type m_val_;
 
 public:
-  static constexpr value_type LIMIT(const value_type& linkVal) noexcept { return linkVal < MIN ? MIN : linkVal > MAX ? MAX : linkVal; };
+  static constexpr value_type LIMIT(const value_type& linkVal)
+  {
+    return linkVal < MIN ? MIN : linkVal > MAX ? MAX : linkVal;
+  }
 
 public:
   BoundedValue() : m_val_() {}
@@ -50,10 +90,13 @@ public:
   friend self& operator-=(self& lhs, const self& rhs) noexcept { return lhs = lhs - rhs; }
   friend self& operator*=(self& lhs, const self& rhs) noexcept { return lhs = lhs * rhs; }
   friend self& operator/=(self& lhs, const self& rhs) noexcept { return lhs = lhs / rhs; }
+
+#if CPP_STANDARD < CXX20
   friend self operator+(const self& lhs, const_ref rhs) noexcept { return {LIMIT(lhs.m_val_ + rhs)}; }
   friend self operator-(const self& lhs, const_ref rhs) noexcept { return {LIMIT(lhs.m_val_ - rhs)}; }
   friend self operator*(const self& lhs, const_ref rhs) noexcept { return {LIMIT(lhs.m_val_ * rhs)}; }
   friend self operator/(const self& lhs, const_ref rhs) noexcept { return {LIMIT(lhs.m_val_ / rhs)}; }
+#endif// _HAS_CXX20
 
   friend bool operator==(const self& lhs, const self& rhs) noexcept { return lhs.m_val_ == rhs.m_val_; }
   friend bool operator!=(const self& lhs, const self& rhs) noexcept { return lhs.m_val_ != rhs.m_val_; }
@@ -77,7 +120,7 @@ public:
   friend bool operator>=(const_ref lhs, const self& rhs) noexcept { return lhs >= rhs.m_val_; }
 
 public:
-  inline value_type linkVal() const { return m_val_; }
+  inline value_type val() const { return m_val_; }
   inline const_ref ref() const { return m_val_; }
   inline references ref() { return m_val_; }
 
